9739945fe9288184d526235f2a46a81a5697d428
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-09-09 19:48:04 -0500

    feat: updated game logic to now work with game state instead of fen

--- src/amazons.ts
+++ src/amazons.ts
@@ -2,17 +2,18 @@ import { INVALID_MOVE } from "boardgame.io/core";
 import { Amazons, DEFAULT_POSITIONS } from "amazons-game-engine";
 
 import type { Game, MoveFn, MoveMap, TurnConfig } from "boardgame.io";
-import type { FEN, Move } from "amazons-game-engine/dist/types";
+import type { FEN, GameState, Move } from "amazons-game-engine/dist/types";
 
-export interface AmazonsState {
-  fen: FEN;
-}
+export type AmazonsState = GameState;
+
 export interface SetupData {
   fen: FEN;
 }
 
-function Load(fen: FEN) {
-  return Amazons(fen, false); // unsafe loading
+function Load(state: GameState): Amazons;
+function Load(fen: FEN): Amazons;
+function Load(param: any) {
+  return new Amazons(param);
 }
 
 export const AmazonsGame: Game<AmazonsState> = {
@@ -23,34 +24,37 @@ export const AmazonsGame: Game<AmazonsState> = {
 
   setup: (_, setupData: SetupData) => {
     // TODO: be careful not to give this an invalid fen! maybe return default if so
+    let amz: Amazons;
     if (setupData?.fen) {
-      return { fen: setupData.fen };
+      amz = Load(setupData.fen);
+      return amz.state();
     }
-    return { fen: DEFAULT_POSITIONS[6]! };
+    amz = Load(DEFAULT_POSITIONS[10]!);
+    return amz.state();
   },
 
   moves: {
     move: ((G, ctx, m: Move) => {
-      const amazons = Load(G.fen);
+      const amz = Load(G);
       if (
-        (amazons.turn() == "w" && ctx.currentPlayer == "1") ||
-        (amazons.turn() == "b" && ctx.currentPlayer == "0")
+        (amz.turn() == "w" && ctx.currentPlayer == "1") ||
+        (amz.turn() == "b" && ctx.currentPlayer == "0")
       ) {
         console.error("wrong player!");
         return INVALID_MOVE;
       }
-      const success = amazons.move(m);
+      const success = amz.move(m);
       if (!success) {
         console.error("move was invalid!");
         return INVALID_MOVE;
       }
-      return { fen: amazons.fen(), last_move: m };
+      return amz.state();
     }) as MoveFn<AmazonsState>,
 
     random_move: ((G) => {
-      const amazons = Load(G.fen);
-      const move = amazons.random_move();
-      return { fen: amazons.fen(), last_move: move };
+      const amz = Load(G);
+      amz.random_move();
+      return amz.state();
     }) as MoveFn<AmazonsState>,
   } as MoveMap<AmazonsState>,
 
@@ -60,7 +64,7 @@ export const AmazonsGame: Game<AmazonsState> = {
   } as TurnConfig,
 
   endIf: (G) => {
-    const amazons = Load(G.fen);
+    const amazons = Load(G);
     if (amazons.game_over()) return amazons.turn(true);
   },
 };
