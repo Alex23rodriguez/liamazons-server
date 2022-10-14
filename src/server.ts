7cc539b738c86f1230b2a9bab92525e23997dd07
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-10-13 21:16:51 -0500

    feat: added database storage

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,15 @@
-/*
- * Copyright 2017 The boardgame.io Authors
- *
- * Use of this source code is governed by a MIT-style
- * license that can be found in the LICENSE file or at
- * https://opensource.org/licenses/MIT.
- */
-
 import { Server, Origins } from "boardgame.io/server";
+import { PostgresStore } from "bgio-postgres";
 import { Game as TicTacToe } from "./tictactoe";
 import { AmazonsGame as Amazons } from "./amazons";
+
+const db = new PostgresStore(process.env.DATABASE_URL);
 
 const PORT = Number(process.env.PORT) || 8000;
 const server = Server({
   games: [TicTacToe, Amazons],
   origins: [/.*/, Origins.LOCALHOST], // TODO: remove wildcard after dev is done!
+  db,
 });
 console.log(`port is ${PORT}`);
 server.run(PORT);
